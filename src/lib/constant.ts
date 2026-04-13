51b3d9f06616f2087d22e6df1bd88b4095b696a8
darkweb199717 <125276209+darkweb199717@users.noreply.github.com>
2026-04-13 12:18:47 +0400

    first commit

---
+++ src/lib/constant.ts
@@ -0,0 +1,7 @@
+export const SITE_URL = "https://www.nwstime.com";
+export const SITE_NAME = "NWS Time";
+export const SITE_TITLE = "NWS Time - Calculate Loans, Investments, and More";
+export const SITE_DESCRIPTION =
+	"Free online finance calculator. Calculate loans, investments, and more instantly. No registration required.";
+export const SITE_AUTHOR = "NWS Time";
+export const SITE_EMAIL = "Tharindulakmal027@gmail.com";
