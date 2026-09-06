52a2f4a08726310283a10f98c23f54573daef935
darkweb199717 <125276209+darkweb199717@users.noreply.github.com>
2026-09-06 20:06:31 +0400

    Update Adsterra_CodeBanner.tsx

--- src/components/Adsterra_CodeBanner.tsx
+++ src/components/Adsterra_CodeBanner.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 
 export default function AdsterraCodeBanner() {
   useEffect(() => {
-    if (typeof window !== 'undefined' && window.atob) {
+    if (typeof window !== 'undefined') {
       const script = document.createElement('script')
       script.src = 'https://gigglehiccup.com/b7494262262e432b34923f2c104e5bc8/invoke.js'
       script.async = true
